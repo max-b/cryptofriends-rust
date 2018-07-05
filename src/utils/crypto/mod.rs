5dd1507a3981edcba2e7c95cbb7612ecc511829a
Maxb <maxb.personal@gmail.com>
2018-07-05 13:06:07 -0400

    Implementing CTR mode seek and edit ciphertext for challenge 25.

--- src/utils/crypto/mod.rs
+++ src/utils/crypto/mod.rs
@@ -184,6 +184,87 @@ pub fn aes_ctr(key: &[u8], input: &[u8], nonce: &[u8]) -> Vec<u8> {
     output
 }
 
+pub fn edit_aes_ctr(ciphertext: &[u8], key: &[u8], nonce: &[u8], offset: usize, newtext: &[u8]) -> Vec<u8> {
+    let block_size = key.len();
+
+    let mut output = Vec::new();
+    let encryptor = aessafe::AesSafe128Encryptor::new(&key);
+
+    output.extend_from_slice(&ciphertext[..]);
+
+    let start_block = offset / block_size;
+    let mut end_of_ciphertext_to_edit = offset + newtext.len();
+    if end_of_ciphertext_to_edit % block_size != 0 {
+        let remainder = block_size - (end_of_ciphertext_to_edit % block_size);
+        if end_of_ciphertext_to_edit + remainder < ciphertext.len() {
+            end_of_ciphertext_to_edit += remainder;
+        }
+    }
+
+    assert!(end_of_ciphertext_to_edit < ciphertext.len());
+
+    let block_offset = offset % block_size;
+
+    let mut block_count = start_block as u64;
+
+    let mut ciphertext_to_edit = Vec::new();
+    ciphertext_to_edit.extend_from_slice(&ciphertext[start_block * block_size..end_of_ciphertext_to_edit]);
+
+    let ciphertext_blocks = ciphertext_to_edit.chunks(block_size);
+
+    let mut keystream = vec![0; block_size];
+
+    for ciphertext_block in ciphertext_blocks {
+        let mut nonce_count = Vec::new();
+
+        nonce_count.extend_from_slice(&nonce[..]);
+        nonce_count
+            .write_u64::<LittleEndian>(block_count)
+            .expect("Error writing count as u64 -> little endian bytes.");
+
+        encryptor.encrypt_block(&nonce_count[..], &mut keystream[..]);
+
+        let mut plaintext = xor(&keystream[0..ciphertext_block.len()], &ciphertext_block[..]);
+        let plaintext_len = plaintext.len();
+
+        let mut newtext_to_splice = Vec::new();
+
+        let newtext_start = match block_count as usize {
+            count if count == start_block => 0,
+            count => ((count * block_size) - offset)
+        };
+
+        let newtext_end = {
+            if block_count as usize == start_block {
+                if block_offset + newtext.len() < block_size {
+                    newtext.len()
+                } else {
+                    (newtext_start + plaintext_len) - block_offset
+                }
+            } else {
+                if newtext_start + plaintext_len > newtext.len() {
+                    newtext.len()
+                } else {
+                    newtext_start + plaintext_len
+                }
+            }
+        };
+
+        newtext_to_splice.extend_from_slice(&newtext[newtext_start..newtext_end]);
+        if block_count as usize == start_block {
+            plaintext.splice(block_offset..newtext_to_splice.len() + block_offset, newtext_to_splice.into_iter());
+        } else {
+            plaintext.splice(0..newtext_to_splice.len(), newtext_to_splice.into_iter());
+        }
+
+        let xor_result = xor(&keystream[0..plaintext_len], &plaintext[..]);
+        output.splice(block_count as usize * block_size..block_count as usize * block_size + xor_result.len(), xor_result.into_iter());
+        block_count += 1;
+    }
+
+    output
+}
+
 pub fn prng_cipher<T: Prng>(seed: u16, input: &[u8]) -> Vec<u8> {
     let mut output = Vec::new();
     let mut prng: T = T::new(seed.into());
@@ -192,4 +273,53 @@ pub fn prng_cipher<T: Prng>(seed: u16, input: &[u8]) -> Vec<u8> {
         output.push(byte ^ keystream_byte);
     }
     output
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use utils::bytes::*;
+
+    #[test]
+    fn aes_ctr_seek_edit() {
+        let key = generate_random_aes_key();
+        let initial_plaintext = "Say -- Play that funky music Say, go white boy, go white boy go
+        play that funky music Go white boy, go white boy, go
+        Lay down and boogie and play that funky music till you die.".as_bytes();
+
+        let nonce: Vec<u8> = vec![0; 8];
+        let initial_ciphertext = aes_ctr(&key[..], &initial_plaintext[..], &nonce[..]);
+
+        let text_to_insert = "HIHIHIHIHIHIHIHI".as_bytes();
+        let offset = 101;
+        let new_ciphertext = edit_aes_ctr(&initial_ciphertext[..], &key[..], &nonce[..], offset, text_to_insert);
+
+        let plaintext = aes_ctr(&key[..], &new_ciphertext[..], &nonce[..]);
+
+        assert_eq!(&plaintext[offset..offset + text_to_insert.len()], &text_to_insert[..]);
+
+        let text_to_insert = "HIHI".as_bytes();
+        let offset = new_ciphertext.len() - text_to_insert.len() - 1;
+        let new_ciphertext = edit_aes_ctr(&initial_ciphertext[..], &key[..], &nonce[..], offset, text_to_insert);
+
+        let plaintext = aes_ctr(&key[..], &new_ciphertext[..], &nonce[..]);
+
+        assert_eq!(&plaintext[offset..offset + text_to_insert.len()], &text_to_insert[..]);
+
+        let text_to_insert = "A".as_bytes();
+        let offset = new_ciphertext.len() - text_to_insert.len() - 1;
+        let new_ciphertext = edit_aes_ctr(&initial_ciphertext[..], &key[..], &nonce[..], offset, text_to_insert);
+
+        let plaintext = aes_ctr(&key[..], &new_ciphertext[..], &nonce[..]);
+
+        assert_eq!(&plaintext[offset..offset + text_to_insert.len()], &text_to_insert[..]);
+
+        let text_to_insert = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA".as_bytes();
+        let offset = new_ciphertext.len() - text_to_insert.len() - 1;
+        let new_ciphertext = edit_aes_ctr(&initial_ciphertext[..], &key[..], &nonce[..], offset, text_to_insert);
+
+        let plaintext = aes_ctr(&key[..], &new_ciphertext[..], &nonce[..]);
+
+        assert_eq!(&plaintext[offset..offset + text_to_insert.len()], &text_to_insert[..]);
+    }
 }
