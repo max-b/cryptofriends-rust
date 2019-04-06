3a7b7ce2a57b86302f8453585f9e8bf766ccd875
Max Bittman <max@npmjs.com>
2019-04-06 14:58:03 -0700

    Cleanup challenge 46; runs in decent time with cargo test --release mode

--- src/set_6/challenge_46.rs
+++ src/set_6/challenge_46.rs
@@ -11,14 +11,9 @@ pub enum Parity {
 pub fn plaintext_parity(ciphertext: &BigUint, rsa: &RSA) -> Parity {
     let decrypt = rsa.decrypt(ciphertext);
 
-    println!("decrypt = {:?}", &decrypt);
-    println!("decrypt bytes = {:?}", &decrypt.to_bytes_be());
-
-    if decrypt & BigUint::from(1 as u32) == BigUint::one() {
-        println!("Odd");
+    if decrypt & BigUint::one() == BigUint::one() {
         Parity::Odd
     } else {
-        println!("Even");
         Parity::Even
     }
 }
@@ -45,18 +40,17 @@ mod tests {
         let mut c = ciphertext.clone();
         let mut higher_bound = rsa.n.clone();
         let mut lower_bound = BigUint::zero().clone();
+        let two_to_e = two.modpow(&rsa.e, &rsa.n);
 
         while &higher_bound > &BigUint::zero() {
-            let tmp = two.modpow(&rsa.e, &rsa.n);
-
-            let c2 = (&c * &tmp) % &rsa.n;
+            let c2 = (&c * &two_to_e) % &rsa.n;
             c = c2;
 
             let parity = plaintext_parity(&c, &rsa);
 
             let diff = &higher_bound - &lower_bound;
             let delta = &diff / &two;
-            if parity == Parity::Odd {
+            if parity == Parity::Even {
                 higher_bound = &higher_bound - &delta;
             } else {
                 lower_bound = &lower_bound + &delta;
@@ -64,7 +58,6 @@ mod tests {
 
             let high_guess_string = bigint::biguint_to_string(&higher_bound);
             let low_guess_string = bigint::biguint_to_string(&lower_bound);
-            println!("actual bytes = {:?}", &base64_to_bytes("VGhhdCdzIHdoeSBJIGZvdW5kIHlvdSBkb24ndCBwbGF5IGFyb3VuZCB3aXRoIHRoZSBGdW5reSBDb2xkIE1lZGluYQ=="));
             println!("guess = {:?}", &high_guess_string);
             println!("guess = {:?}", &low_guess_string);
             if high_guess_string.contains(actual_plaintext) {
